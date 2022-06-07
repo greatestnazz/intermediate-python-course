c6ae3bc6b8d8aab882d956b3074e5551e6c95b06
nasir <mnasshuaib@gmail.com>
2022-06-07 15:16:34 +0100

    User can input dice sides and number of sides

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,12 @@
 import random
 
 #Variable defination
-dicerolls = 2
+dicerolls = int(input("How many dice do you have: "))
 sum_dice = 0
-
+dice_sides = int(input("How many sides do the dice have eg 1,6,12: "))
 #loop to iterate and add the values of the dice rolls
 for i in range(0, dicerolls):
-     roll = random.randint(1, 6)
+     roll = random.randint(1, dice_sides)
      sum_dice += roll
      print(f'You rolled a {roll}')
 
