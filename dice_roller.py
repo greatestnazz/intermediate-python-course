25da1c52f8c496e00a105fe346575510a9c083b8
nasir <mnasshuaib@gmail.com>
2022-06-07 11:39:17 +0100

    added commit to the code

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,18 @@
 import random
 
+#Variable defination
 dicerolls = 2
 sum_dice = 0
 
+#loop to iterate and add the values of the dice rolls
 for i in range(0, dicerolls):
      roll = random.randint(1, 6)
      sum_dice += roll
      print(f'You rolled a {roll}')
-print(f'You have rolled a total of {sum_dice}')
+
+#Conditional to print comments with the rolls
+if sum_dice <= 6 :
+    print(f'You have rolled a total of {sum_dice}, Big fail')
+else:
+    print(f'You have rolled a total of {sum_dice}, Whopee')
   
